e63f3704ad7754794e60a62bed1a543a85d68c78
cshimvin <>
2024-01-05 16:14:41 +0000

    Add category and user pages

--- app.py
+++ app.py
@@ -14,43 +14,29 @@ app = Flask(__name__)
 # Configure Flask and MongoDB
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 
-MONGO_URI = os.environ.get("MONGO_URI")
-MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
-MONGO_COLLECTION = "restaurants"
-
-
-def mongo_connect(url):
-    try:
-        conn = pymongo.MongoClient(url)
-        print("Mongo is connected")
-        return conn
-    except pymongo.errors.ConnectionFailure as e:
-        print("Could not connect to MongoDB: %s") % e
-
-
 @app.route("/")
+@app.route("/get_restaurants")
 def index():
-    conn = mongo_connect(MONGO_URI)
-    coll = conn[MONGO_DBNAME][MONGO_COLLECTION]
-    documents = coll.find()
-    # for doc in documents:
-    #    print(doc)
-    restaurants = list(mongo.db.restaurants.find())
-    return render_template("index.html", documents=documents)
+    restaurants = mongo.db.restaurants.find()
+    return render_template("index.html", restaurants=restaurants)
+
+
+@app.route("/categories")
+def get_categories():
+    return render_template("categories.html")
+
+
+@app.route("/users")
+def get_users():
+    return render_template("users.html")
 
 
 # set debug to false when operational
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=os.environ.get("PORT"), debug=True)
-
-
-
-
-
-
