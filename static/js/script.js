79fd497c5ee616987955d32b8e0312e7ecfe5403
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-01-23 16:08:09 +0000

    Add jQuery, custom scripts and tooltips on registration form

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// Function to show tooltips
+// Taken from Bootstrap documents - https://getbootstrap.com/docs/4.0/components/tooltips/
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
