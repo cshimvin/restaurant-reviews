305a585c684a9b98c0a73925522db9b6cb98e953
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-01-23 16:40:19 +0000

    Update bootstrap and add favicon

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,4 @@
 // Function to show tooltips
 // Taken from Bootstrap documents - https://getbootstrap.com/docs/4.0/components/tooltips/
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
